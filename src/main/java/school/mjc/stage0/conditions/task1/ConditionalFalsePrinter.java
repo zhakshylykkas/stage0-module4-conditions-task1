da98a2cc5c855453f468cead0dc405446f297453
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-09-02 02:10:41 +0600

    Changes committed after the second testing

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
-        if (-1 > 0) {
+        if (false) {
 
         } else {
             System.out.println(false);
--- src/main/java/school/mjc/stage0/conditions/task1/NoBracketsPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/NoBracketsPrinter.java
@@ -2,10 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class NoBracketsPrinter {
     public static void main(String[] args) {
-        int positiveNum = 9;
-        if(positiveNum < 0)
+        if(false) {
             System.out.println("The statement is true");
-        else if (positiveNum == 0) System.out.println("I'm out of checking");
+            System.out.println("I'm out of checking");
+        }
 
     }
 }
